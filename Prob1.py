8cc95cd50f83fb0831303130e63bd6f67ceab64a
bjlivengood <bjlivengood@willamette.edu>
2024-09-16 22:06:46 -0700

    Update Prob1.py

--- Prob1.py
+++ Prob1.py
@@ -1,9 +1,9 @@
 #====================================================
 # Filename: Prob1.py
 # 
-# Your name:
-# Who did you work with (if anyone)?:
-# Estimate for time spent (in hrs)?:
+# Your name: Baily LIvengood
+# Who did you work with (if anyone)?: Nobody
+# Estimate for time spent (in hrs)?: 5
 #====================================================
 
 import karel
@@ -13,10 +13,50 @@ import karel
 # get you started, but you can (and should) add whatever
 # other helper functions you want below.
 
-def create_checkerboard():
-    """ Main function to create the checkerboard pattern. """
-    # You need to add code here
 
+
+def main():
+    put_beeper()
+    Check_right()
+    
+
+def Check_right(): #Complete a row going right
+    Checker()
+    turn_left()
+    if front_is_clear():
+        start_line()
+        turn_left()
+        Check_left()
+
+def Check_left(): #Complete a row going left
+    Checker()
+    turn_right()
+    if front_is_clear():
+        start_line()
+        turn_right()
+        Check_right()
+
+def start_line(): 
+    if beepers_present():
+        move()
+    else:
+        move()
+        put_beeper()
+
+
+def Checker(): #Alternate beeper placement
+    while front_is_clear():
+        if beepers_present():
+            move()
+        else:
+            move()
+            put_beeper()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
 # Remember to define any more helper functions you want down here
 
