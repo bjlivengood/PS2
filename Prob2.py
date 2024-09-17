4036d21d2c601e82851085309438e469aee4d48f
bjlivengood <bjlivengood@willamette.edu>
2024-09-16 23:42:11 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,15 +1,23 @@
 #====================================================
 # Filename: Prob2.py
 # 
-# Your name:
-# Who did you work with (if anyone)?:
-# Estimate for time spent (in hrs)?:
+# Your name: Baily Livengood
+# Who did you work with (if anyone)?: Nobody
+# Estimate for time spent (in hrs)?: 3
 #====================================================
 
 # Define your function here
-
-
-
+def divisible_by_six_or_seven(x, y):
+    for i in range(40,60):
+        if i % 6 == 0:
+            if not i % 7 == 0:
+                print(i)
+        if i % 7 == 0:
+            if not i % 6 == 0:
+                print(i)
+    if divisible_by_six_or_seven == True:
+        return count + 1 # Couldnt figure out how to return a count
+    
 
 
 
@@ -21,5 +29,3 @@ if __name__ == '__main__':
     # Same basic testing here, but you should test MORE!
     count = divisible_by_six_or_seven(40,60)
     print(count)
-
-
